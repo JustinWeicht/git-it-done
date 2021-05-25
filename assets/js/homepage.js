5eb03d67ba1c41631b6a5a79ddf390f22ad08737
Justin Weicht <justinweicht11@gmail.com>
2021-05-25 08:10:57 -0400

    add error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,12 +2,14 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 
-// variables for repository column
+// variables for repository display
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
 // function to get user input from form
 var formSubmitHandler = function(event) {
+    // prevent page from reloading when submit button is clicked
+
     event.preventDefault();
     // get value from input element
     var username = nameInputEl.value.trim();
@@ -32,16 +34,33 @@ var getUserRepos = function(user) {
     // make request to the url
     fetch(apiUrl).then(function(response) {
 
-        // parse the response into a javascript array
-        response.json().then(function(data) {
-            // send converted data from getUserRepos to displayRepos
-            displayRepos(data, user);
-        });
+        // request was successful
+        if (response.ok) {
+            // parse the response into a javascript array
+            response.json().then(function(data) {
+                // send converted data from getUserRepos to displayRepos
+                displayRepos(data, user);
+            });            
+        } else {
+            // alert the user that the GitHub user does not exist
+            alert("Error: GitHub User Not Found");
+        }
+    })
+    // if '.then' method fails, fetch() will use the '.catch' method below
+    .catch(function(error) {
+        // notice the '.catch()' getting chaines onto the end of the '.then' method
+        alert("Unable to connet to GitHub");
     });
 };
 
 // function to accept both the array and term we searched for as parameters
 var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+
     // clear old data
     repoContainerEl.textContent = "";
 
@@ -70,8 +89,10 @@ var displayRepos = function(repos, searchTerm) {
 
         // check if current repo has issues or not
         if (repos[i].open_issues_count > 0) {
+            // apply red 'X' if there are issues
             statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
         } else {
+            // apply blue '√' if there are no issues
             statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
         }
 
